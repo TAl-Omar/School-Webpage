16443c34027ed0c1f700de3022afe57f92d17002
TAlOmar <>
2021-04-14 11:50:16 +0000

    neww file

---
+++ Javascript1.js
@@ -0,0 +1,27 @@
+
+var Username = 'Welcome to ESP, this site uses cookies'
+alert('Welcome to ESP')
+
+
+var UserCity = prompt('please enter your city, Irbid or Amman')
+console.log(UserCity); 
+
+if(UserCity == 'Irbid' || UserCity == 'irbid'){
+  console.log('Irbid');
+} 
+else if(UserCity == 'Amman' || UserCity == 'amman'){
+  console.log('Amman');
+}
+
+
+var Name = prompt('please enter whether your Parent or a Student')
+alert('Welcome ' + Name); 
+
+if(Name == 'Parent' || Name == 'parent'){
+  console.log('Parent');
+} 
+else if(Name == 'Student' || Name == 'student'){
+  console.log('Student');
+}
+
+
